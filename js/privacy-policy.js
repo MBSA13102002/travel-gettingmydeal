0aba8d1aa285848e0f1ced96282200af51fa207e
mbsa13102002@gmail.com <mbsaiaditya@gmail.com>
2021-06-27 17:22:09 +0530

    commit

--- js/privacy-policy.js
+++ js/privacy-policy.js
@@ -1,3 +1,9 @@
+function Call_Clicks() {
+    let CallClicks_New = firebase.database().ref("Call_Clicks_ViewInfo");
+    CallClicks_New.set({
+        amount: window.callclicks + 1,
+    });
+}
 let SticyCallButton = firebase.database().ref("Visibility_Section").child("Sticky-Call-Button");
 SticyCallButton.on('value', (snapshot) => {
     const data = snapshot.val();
